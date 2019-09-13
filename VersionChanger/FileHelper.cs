30735f551a499fcefbc5caf376d4b4d11f2040ad
Andreas Pouwels <apouwels85@gmail.com>
2019-09-13 20:40:48 +0200

    Initial commit

---
+++ VersionChanger/FileHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VersionChanger
+{
+    /// <summary>
+    /// Provides several functions to interact with files
+    /// </summary>
+    internal static class FileHelper
+    {
+        /// <summary>
+        /// Contains the regex for the version number
+        /// </summary>
+        private static readonly Regex VersionNumberRegex = new Regex(@"\d+\.{1}\d+\.{1}\d+\.{1}\d+");
+
+        /// <summary>
+        /// Gets the path of a specified file starting from the given directory
+        /// </summary>
+        /// <param name="filename">The name of the file</param>
+        /// <param name="directory">The path of the start directory (optional, when empty the current directory will be chosen)</param>
+        /// <param name="pattern">The search pattern (optional)</param>
+        /// <returns>The full path of the file to be searched for</returns>
+        public static string GetFile(string filename, string directory = "", string pattern = "*.*")
+        {
+            if (string.IsNullOrEmpty(directory))
+                directory = Global.GetBaseFolder();
+
+            var dirInfo = new DirectoryInfo(directory);
+            if (!dirInfo.Exists)
+                return "";
+
+            var files = dirInfo.GetFiles(pattern, SearchOption.AllDirectories);
+
+            var file = files.FirstOrDefault(f => f.FullName.ContainsIgnoreCase(filename));
+            if (file == null)
+                return dirInfo.Parent != null ? GetFile(filename, dirInfo.Parent.FullName, pattern) : "";
+
+            return file.FullName;
+        }
+
+        /// <summary>
+        /// Changes the content of the given file according to the given regex pattern and the replace string
+        /// </summary>
+        /// <param name="file">The path of the file</param>
+        /// <param name="version">The new version number</param>
+        /// <returns>true when successful, otherwise false</returns>
+        /// <exception cref="ArgumentNullException">Will be thrown when the file or pattern parameter is empty</exception>
+        /// <exception cref="FileNotFoundException">Will be thrown when the file doesn't exist</exception>
+        public static bool ChangeFileContent(string file, string version)
+        {
+            if (string.IsNullOrEmpty(file))
+                throw new ArgumentNullException(nameof(file));
+
+            if (!File.Exists(file))
+                throw new FileNotFoundException("The given file doesn't exist.", file);
+
+            if (string.IsNullOrEmpty(version))
+                throw new ArgumentNullException(nameof(version));
+
+            var content = File.ReadAllText(file);
+            if (string.IsNullOrEmpty(content))
+                return false;
+
+            content = VersionNumberRegex.Replace(content, version);
+
+            File.WriteAllText(file, content);
+
+            return File.Exists(file);
+        }
+
+        /// <summary>
+        /// Gets the current version number
+        /// </summary>
+        /// <param name="file">The path of the file</param>
+        /// <returns>The current version number</returns>
+        /// <exception cref="ArgumentNullException">Will be thrown when the file parameter is empty</exception>
+        /// <exception cref="FileNotFoundException">Will be thrown when the file doesn't exist</exception>
+        public static Version GetCurrentVersion(string file)
+        {
+            if (string.IsNullOrEmpty(file))
+                throw new ArgumentNullException(nameof(file));
+
+            if (!File.Exists(file))
+                throw new FileNotFoundException("The given file doesn't exist.", file);
+
+            var content = File.ReadAllText(file);
+            if (string.IsNullOrEmpty(content))
+                return new Version();
+
+            var version = VersionNumberRegex.Match(content).Value;
+
+            try
+            {
+                return Version.Parse(version);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Error.WriteLine($"Negative value in {version}");
+                return new Version();
+            }
+            catch (ArgumentException)
+            {
+                Console.Error.WriteLine($"Bad number of components in {version}");
+                return new Version();
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine($"Non-integer value in {version}");
+                return new Version();
+            }
+            catch (OverflowException)
+            {
+                Console.Error.WriteLine($"Number out of range in {version}");
+                return new Version();
+            }
+        }
+    }
+}
---
+++ VersionChanger/Global.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace VersionChanger
+{
+    /// <summary>
+    /// Provides several useful functions
+    /// </summary>
+    internal static class Global
+    {
+        /// <summary>
+        /// Gets the path of the base folder
+        /// </summary>
+        /// <returns>The path of the base folder</returns>
+        public static string GetBaseFolder()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        /// <summary>
+        /// Search for a substring in a string and returns true if the substring was found
+        /// </summary>
+        /// <param name="value">The original string</param>
+        /// <param name="substring">The substring</param>
+        /// <returns>true if the substring was found, otherwise false</returns>
+        public static bool ContainsIgnoreCase(this string value, string substring)
+        {
+            if (string.IsNullOrEmpty(substring))
+                return false;
+
+            return value.IndexOf(substring, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        /// <summary>
+        /// Converts the given string into an int value. When the string value is not a valid int, a 0 will be returned
+        /// </summary>
+        /// <param name="value">The string value</param>
+        /// <returns>The converted int value</returns>
+        public static int ToInt(this string value)
+        {
+            return int.TryParse(value, out var result) ? result : 0;
+        }
+
+        /// <summary>
+        /// Extracts the shipped version
+        /// </summary>
+        /// <param name="args">The shipped arguments</param>
+        /// <returns>The version</returns>
+        public static Version ExtractVersion(string[] args)
+        {
+            // Check if something was transferred
+            if (!args.Any())
+                return null;
+
+            var major = 0;
+            var minor = 0;
+            var build = 0;
+            var revision = 0;
+            foreach (var argument in args)
+            {
+                if (argument.ContainsIgnoreCase("-ma="))
+                    major = argument.Replace("-ma=", "").ToInt();
+
+                if (argument.ContainsIgnoreCase("-mi="))
+                    minor = argument.Replace("-mi=", "").ToInt();
+
+                if (argument.ContainsIgnoreCase("-b="))
+                    build = argument.Replace("-b=", "").ToInt();
+
+                if (argument.ContainsIgnoreCase("-r="))
+                    revision = argument.Replace("-r=", "").ToInt();
+
+                // A complete version number is given
+                if (argument.Contains("."))
+                    return ExtractVersion(argument);
+            }
+
+            return new Version(major, minor, build, revision);
+        }
+
+        /// <summary>
+        /// Extracts a given version number
+        /// </summary>
+        /// <param name="version">The version number</param>
+        /// <returns>The version</returns>
+        private static Version ExtractVersion(string version)
+        {
+            if (string.IsNullOrEmpty(version))
+                return null;
+
+            var content = version.Split(new[] {"."}, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (content.Length)
+            {
+                case 4:
+                    return new Version(content[0].ToInt(), content[1].ToInt(), content[2].ToInt(), content[3].ToInt());
+                case 3:
+                    return new Version(content[0].ToInt(), content[1].ToInt(), content[2].ToInt());
+                case 2:
+                    return new Version(content[0].ToInt(), content[1].ToInt());
+                case 1:
+                    return new Version(content[0].ToInt(), 0);
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new version number
+        /// </summary>
+        /// <returns>The version number</returns>
+        public static Version CreateVersion()
+        {
+            var year = DateTime.Now.ToString("yy").ToInt();
+            var week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstDay,
+                DayOfWeek.Monday);
+
+            return new Version(year, week, 0, (int) DateTime.Now.TimeOfDay.TotalMinutes);
+        }
+    }
+}
